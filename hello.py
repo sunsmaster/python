a3476e4faaf8c9a6429f586c3566b23ed628eab9
Elias Englsperger <elen0223@storsjogymnasiet.se>
2023-09-05 12:21:28 +0200

    mums

--- hello.py
+++ hello.py
@@ -1,1 +1,24 @@
-print("cu")
+import random
+print("\n-------------------------------------------------------------------")
+print(".:gissa talet :.\n\n")
+print("gissa ett tal mellan 1 och 10 och pröva lyckan, du får 3st försök!\n")
+slumptal = random.randint(1,10)
+#print (slumptal)
+i = 0
+hittat = False
+#loop
+while i < 3:
+    gissatal =input ("mata in tal: ")
+
+    if  slumptal == int(gissatal):
+        hittat = True
+        print("\n rätt svar! \n")
+        break
+
+    i += 1
+
+    if hittat:
+        print("\nBra jobbat, här får du fem kr")
+    else:
+        print("\nBättre lycka nästa gång")
+print("\n---------------------------------------------------------------------")
